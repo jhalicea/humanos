9e57882b6f6572ddd416cf43107a9307d59db80d
Jon Alicea <me@jhalicea.com>
2026-09-10 00:57:00 -0400

    Assert broker manifest extensions fail closed

--- tests/test_swarm_runtime_router_failclosed.py
+++ tests/test_swarm_runtime_router_failclosed.py
@@ -5,10 +5,12 @@ from swarm_runtime import SwarmRuntime
 
 
 class RouterFailClosedTests(unittest.TestCase):
-    def test_runtime_rejects_uninstalled_provider_before_run(self):
+    def test_runtime_rejects_undeclared_provider_field_in_broker_manifest(self):
         with tempfile.TemporaryDirectory() as root:
-            value = config(root); value['broker']['agents'][0]['provider'] = 'uninstalled'
-            with self.assertRaisesRegex(ValueError, 'Unsupported'): SwarmRuntime(value)
+            value = config(root)
+            value['broker']['agents'][0]['provider'] = 'uninstalled'
+            with self.assertRaisesRegex(ValueError, 'Unexpected or missing fields'):
+                SwarmRuntime(value)
 
 
 if __name__ == '__main__': unittest.main()
